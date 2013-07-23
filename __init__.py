68819c39a7b7f56e64b14c0c43a9d9239e73ce0f
Anders Petersson <me@anderspetersson.se>
2013-07-23 16:44:36 +0200

    Initial commit.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+VERSION = (0, 1, 0, 'alpha', 0)
+
+def get_version(*args, **kwargs):
+    # Don't litter django/__init__.py with all the get_version stuff.
+    # Only import if it's actually called.
+    from bestofblocket.core.utils import get_version
+    return get_version(*args, **kwargs)
