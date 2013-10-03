9d13e27e5d8ee02d933238e1772476bb1a19b028
Anders Petersson <me@anderspetersson.se>
2013-10-03 19:32:08 +0200

    Add search_fields to Ads Admin page.

--- bestofblocket/core/admin.py
+++ bestofblocket/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from bestofblocket.core.models import Ad
 
+class AdAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'filnamn']
 
-admin.site.register(Ad)
+admin.site.register(Ad, AdAdmin)
