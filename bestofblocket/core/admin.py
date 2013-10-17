ae2f55a4d1e4074b9b909ffac82084200d939de0
Anders Petersson <me@anderspetersson.se>
2013-10-17 23:52:59 +0200

    Enable admin for Link tips.

--- bestofblocket/core/admin.py
+++ bestofblocket/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from bestofblocket.core.models import Ad
+from bestofblocket.core.models import Ad, Link
 
 class AdAdmin(admin.ModelAdmin):
     search_fields = ['title', 'filnamn']
 
 admin.site.register(Ad, AdAdmin)
+admin.site.register(Link)
