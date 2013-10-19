3f2a235bd774feb583c37d9a5095ae68c4a50595
Anders Petersson <me@anderspetersson.se>
2013-10-19 11:46:27 +0200

    Add prepopulate field for slug in admin.

--- bestofblocket/core/admin.py
+++ bestofblocket/core/admin.py
@@ -3,6 +3,7 @@ from bestofblocket.core.models import Ad, Link
 
 class AdAdmin(admin.ModelAdmin):
     search_fields = ['title', 'filnamn']
+    prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Ad, AdAdmin)
 admin.site.register(Link)
