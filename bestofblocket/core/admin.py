5bdc80f730803bf45fe58e4386b1ab0a2f759087
Anders Petersson <me@anderspetersson.se>
2013-11-04 12:07:39 +0100

    Remove filnamn field from searchable_fields in admin.

--- bestofblocket/core/admin.py
+++ bestofblocket/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from bestofblocket.core.models import Ad, Link
 
 class AdAdmin(admin.ModelAdmin):
-    search_fields = ['title', 'filnamn']
+    search_fields = ['title', 'slug']
     prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Ad, AdAdmin)
