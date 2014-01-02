e81381d3c0e38e77f37a5f5867db98b4635e9cb1
Anders Petersson <me@anderspetersson.se>
2014-01-02 22:03:12 +0100

    Show version of content in admin list.

--- bestofblocket/core/admin.py
+++ bestofblocket/core/admin.py
@@ -4,7 +4,7 @@ from bestofblocket.core.models import Ad, Link
 class AdAdmin(admin.ModelAdmin):
     search_fields = ['title', 'slug']
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('title', 'is_approved')
+    list_display = ('title', 'is_approved', 'generation')
 
 admin.site.register(Ad, AdAdmin)
 admin.site.register(Link)
