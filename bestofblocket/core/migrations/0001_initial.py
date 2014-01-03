8625e0a8526292cc13afaf065d1b648be73cca1e
Anders Petersson <me@anderspetersson.se>
2014-01-03 21:26:21 +0100

    clean up migrations.

--- bestofblocket/core/migrations/0001_initial.py
+++ bestofblocket/core/migrations/0001_initial.py
@@ -12,26 +12,47 @@ class Migration(SchemaMigration):
         db.create_table(u'core_ad', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=255, null=True, blank=True)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
-            ('date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('generation', self.gf('django.db.models.fields.SmallIntegerField')(default=0, null=True, blank=True)),
+            ('is_approved', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('text', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
         ))
         db.send_create_signal(u'core', ['Ad'])
+
+        # Adding model 'Link'
+        db.create_table(u'core_link', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+        ))
+        db.send_create_signal(u'core', ['Link'])
 
 
     def backwards(self, orm):
         # Deleting model 'Ad'
         db.delete_table(u'core_ad')
 
+        # Deleting model 'Link'
+        db.delete_table(u'core_link')
+
 
     models = {
         u'core.ad': {
-            'Meta': {'object_name': 'Ad'},
-            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'Meta': {'ordering': "['-pk']", 'object_name': 'Ad'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'generation': ('django.db.models.fields.SmallIntegerField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'is_approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'core.link': {
+            'Meta': {'object_name': 'Link'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
         }
     }
 
