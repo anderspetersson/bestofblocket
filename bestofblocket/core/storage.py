0abd88de949cc70c3bfdf63866b3e125cc510cb3
Anders Petersson <me@anderspetersson.se>
2018-06-05 13:44:27 +0200

    Removed pipeline.

--- bestofblocket/core/storage.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.staticfiles.storage import CachedFilesMixin
-
-from pipeline.storage import PipelineMixin
-
-from storages.backends.s3boto import S3BotoStorage
-
-
-class S3PipelineCachedStorage(PipelineMixin, CachedFilesMixin, S3BotoStorage):
-    pass
