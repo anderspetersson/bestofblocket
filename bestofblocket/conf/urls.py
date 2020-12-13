290ad49746107461e756bf4f9fd3b1ee92b2e329
Anders Petersson <me@anderspetersson.se>
2020-12-13 16:34:11 +0100

    Added ads.txt.

--- bestofblocket/conf/urls.py
+++ bestofblocket/conf/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^tipsa/', SubmitLinkView.as_view(), name='submit'),
     url(r'^tack/', ThankView.as_view(), name='thanks'),
     url(r'^robots\.txt$', TextTemplateView.as_view(template_name="robots.txt")),
+    url(r'^ads\.txt$', TextTemplateView.as_view(template_name="ads.txt")),
     url(r'(?P<slug>[^/]+)/$', AdView.as_view(), name='ad'),
 ]
