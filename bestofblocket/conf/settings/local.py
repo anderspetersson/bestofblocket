f8b8ad6bcc4c003c6da2741d9b079f244d8687a0
Anders Petersson <me@anderspetersson.se>
2023-05-20 13:58:34 +0200

    Added logging.

--- bestofblocket/conf/settings/local.py
+++ bestofblocket/conf/settings/local.py
@@ -16,9 +16,5 @@ DATABASES = {
     }
 }
 
-# Cloud Storage
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
--- bestofblocket/conf/urls.py
+++ bestofblocket/conf/urls.py
@@ -16,14 +16,13 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='index'),
     path('api/', JSONListView.as_view(), name='jsonindex'),
-    path('mobile/', MobileWebsiteView.as_view(), name='mobile'),
     path('slumpa/gen3/', RandomGen3AdView.as_view(), name='randomgen3'),
     path('slumpa/', RandomAdView.as_view(), name='random'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
     path('tipsa/', SubmitLinkView.as_view(), name='submit'),
     path('tack/', ThankView.as_view(), name='thanks'),
     path('robots.txt', TextTemplateView.as_view(template_name="robots.txt")),
-    path('ads\.txt', TextTemplateView.as_view(template_name="ads.txt")),
+    path('ads.txt', TextTemplateView.as_view(template_name="ads.txt")),
     path('<slug:slug>/', AdView.as_view(), name='ad'),
 ]
 
