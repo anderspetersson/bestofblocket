28d9661947c0eade17dc6605f332f495faceac31
Anders Petersson <me@anderspetersson.se>
2023-05-25 14:11:31 +0200

    Added app privacy policy.

--- bestofblocket/conf/urls.py
+++ bestofblocket/conf/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from bestofblocket.core.views import HomePageView, AdView, RandomAdView, \
  AdSitemap, SubmitLinkView, ThankView, TextTemplateView, AdsListJsonView, \
@@ -22,8 +23,9 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
     path('tipsa/', SubmitLinkView.as_view(), name='submit'),
     path('tack/', ThankView.as_view(), name='thanks'),
-    path('robots.txt', TextTemplateView.as_view(template_name="robots.txt")),
-    path('ads.txt', TextTemplateView.as_view(template_name="ads.txt")),
+    path('robots.txt', TextTemplateView.as_view(template_name='robots.txt')),
+    path('ads.txt', TextTemplateView.as_view(template_name='ads.txt')),
+    path('privacypolicy/', TemplateView.as_view(template_name='app_privacy_policy.html'),),
     path('<slug:slug>/', AdView.as_view(), name='ad'),
 ]
 
