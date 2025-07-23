9d78ebf014ea657a52356b8eeaa4c6822d530fab
Anders Petersson <me@anderspetersson.se>
2025-07-23 20:29:02 +0200

    Added a basic testcase to fetch index page.

---
+++ bestofblocket/core/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase, Client
+
+class TestURLsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_index(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
