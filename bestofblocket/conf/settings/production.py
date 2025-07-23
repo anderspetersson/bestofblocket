b993a91832bd970cd06ef30a6d6bc370942b199d
Anders Petersson <me@anderspetersson.se>
2025-07-23 12:15:56 +0200

    Added settings/production.py after removing its git history to prevent secret leaks.

---
+++ bestofblocket/conf/settings/production.py
@@ -0,0 +1,48 @@
+from .base import *
+import os
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bestofblocketprod',
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_SERVICE_HOST'),
+        'PORT': 5432,
+        "OPTIONS": {
+            "pool": {
+                "min_size": 2,
+                "max_size": 4,
+                "timeout": 10,
+            }
+        },
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+        }
+    }
+}
+
+PREPEND_WWW = True
