ed5961e9e7dfa04610fd0ae27fe1c38a5589641e
Anders Petersson <me@anderspetersson.se>
2025-07-25 15:16:29 +0200

    Added a testcase for detail page.

--- bestofblocket/core/tests.py
+++ bestofblocket/core/tests.py
@@ -2,9 +2,16 @@ from django.test import Client, TestCase
 
 
 class TestURLsTestCase(TestCase):
+    fixtures = ["testdata.json"]
+
     def setUp(self):
         self.client = Client()
 
     def test_get_index(self):
         response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_detail_page(self):
+        response = self.client.get("/945-ltt-olivgron-glastaklucka/")
+        self.assertContains(response, "Avgas bristfällig upphängning")
         self.assertEqual(response.status_code, 200)
